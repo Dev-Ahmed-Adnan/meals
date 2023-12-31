3d8b418ef66066106c7a382f9cd925f823a363ac
Dev-ahmed-adnan <dev.ahmed.adnan@gmail.com>
2023-12-31 16:25:53 +0200

    bugfix

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,98 +12,97 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   var nameController = TextEditingController();
   var passwordController = TextEditingController();
-  String? error;
+  final _formKey = GlobalKey<FormState>();
 
   void _handleLogin() async {
-    if (nameController.text.isEmpty || passwordController.text.isEmpty) {
-      error = "Please fill missing fields!";
-      return;
+    if (_formKey.currentState!.validate()) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      await prefs.setString("name", nameController.text);
+
+      if (!context.mounted) return;
+
+      Navigator.push(context, MaterialPageRoute(builder: (ctx) => const CategoriesPage()));
     }
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    await prefs.setString("name", nameController.text);
-
-    Navigator.push(context, MaterialPageRoute(builder: (ctx) => const CategoriesPage()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Hero(
-              tag: "heroImage",
-              // child: Container(
-              //   width: 200,
-              //   height: 200,
-              //   color: Colors.blue,
-              // ),
-              child: Image.asset(
-                "assets/logo2.png",
-                width: double.infinity,
-                height: 300,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-            Text(
-              "Welcome to Meals App!",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                    color: Colors.white,
-                  ),
-            ),
-            const SizedBox(height: 40),
-            TextFormField(
-              controller: nameController,
-              style: const TextStyle(color: Colors.white),
-              decoration: const InputDecoration(
-                label: Text(
-                  "Password",
-                  style: TextStyle(color: Colors.white),
-                ),
-                hintStyle: TextStyle(color: Colors.white),
-              ),
-              // validator: (value) {
-              //   if (value!.isEmpty) {
-              //     return "Please Enter Name!";
-              //   } else {
-              //     return null;
-              //   }
-              // },
-            ),
-            // Text(nameController.text),
-            TextFormField(
-              obscureText: true,
-              controller: passwordController,
-              style: const TextStyle(color: Colors.white),
-              decoration: const InputDecoration(
-                label: Text(
-                  "Password",
-                  style: TextStyle(color: Colors.white),
-                ),
-                hintStyle: TextStyle(color: Colors.white),
-              ),
-            ),
-            if (error != null) ...[
-              const SizedBox(height: 20),
-              Text(
-                error!,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: Theme.of(context).colorScheme.error,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Hero(
+                    tag: "heroImage",
+                    child: Image.asset(
+                      "assets/logo2.png",
+                      width: double.infinity,
+                      height: 300,
+                      color: Theme.of(context).colorScheme.primary,
                     ),
+                  ),
+                  Text(
+                    "Welcome to Meals App!",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  const SizedBox(height: 40),
+                  TextFormField(
+                    controller: nameController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      label: Text(
+                        "Email or Name",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Name!";
+                      }
+                      return null;
+                    },
+                  ),
+                  // Text(nameController.text),
+                  TextFormField(
+                    obscureText: true,
+                    controller: passwordController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      label: Text(
+                        "Password",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter Password!";
+                      } else if (value.trim().length < 6) {
+                        return "Password must be more than 6 characters!";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 70),
+                  OutlinedButton(
+                    onPressed: _handleLogin,
+                    child: const Text("Login"),
+                  ),
+                ],
               ),
-            ],
-            const SizedBox(height: 70),
-            OutlinedButton(
-              onPressed: _handleLogin,
-              child: const Text("Login"),
             ),
-          ],
+          ),
         ),
       ),
     );
